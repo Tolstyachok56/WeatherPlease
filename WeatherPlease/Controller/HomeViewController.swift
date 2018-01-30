8cb67364c3d92b8b3d53d5600f3590d2a53a3dda
Viktoriia Badisova <vbadisova@gmail.com>
2018-01-30 17:38:10 +0300

    json parsing, update ui

--- WeatherPlease/Controller/HomeViewController.swift
+++ WeatherPlease/Controller/HomeViewController.swift
@@ -14,15 +14,18 @@ import SwiftyJSON
 final class HomeViewController: UIViewController {
     
     //MARK: - Variables
-    private let WEATHER_URL = "api.openweathermap.org/data/2.5/weather"
+    private let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather/"
     private let APP_ID = "6f3162859065dbac6ceb0d7e8ff4fb98"
     
     private let locationManager = CLLocationManager()
-
+    let weatherDataModel = WeatherDataModel()
+    
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var weatherImage: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var windLabel: UILabel!
+    
+    @IBOutlet weak var refreshButton: UIButton!
     
     //MARK: - VC Lifecycle
     
@@ -36,15 +39,45 @@ final class HomeViewController: UIViewController {
     private func getWeatherData(fromURL url: String, withParameters parameters: [String: String]) {
         Alamofire.request(url, method: .get, parameters: parameters).responseJSON { (response) in
             if response.result.isSuccess {
-                print("Success. Got the weather data")
-                
+                //print("Success. Got the weather data")
+                let weatherJSON: JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
             } else {
-                print("Error: \(response.result.error)")
+                print("Error: \(response.result.error!)")
                 self.locationLabel.text = "Connection issues"
             }
         }
     }
     
+    //MARK: - JSON parsing
+    
+    private func updateWeatherData(json: JSON) {
+        if let temperatureResult = json["main"]["temp"].double {
+            weatherDataModel.temperature = Int(temperatureResult - 273.15)
+            weatherDataModel.locationName = json["name"].stringValue
+            weatherDataModel.condition = json["weather"][0]["id"].intValue
+            weatherDataModel.windSpeed = json["wind"]["speed"].intValue
+            weatherDataModel.weatherImageName = weatherDataModel.updateWeatherImage(forConditionID: weatherDataModel.condition)
+            updateUIWithWeatherData()
+        } else {
+            locationLabel.text = "Weather unavailable"
+        }
+    }
+    
+    //MARK: - Update UI
+    
+    private func updateUIWithWeatherData() {
+        locationLabel.text = weatherDataModel.locationName
+        weatherImage.image = UIImage(named: weatherDataModel.weatherImageName)
+        temperatureLabel.text = "\(weatherDataModel.temperature) ºC"
+        windLabel.text = "\(weatherDataModel.windSpeed) m/s"
+    }
+    
+    // MARK: - Refresh
+    
+    @IBAction func refresh(_ sender: UIButton) {
+        locationManager.startUpdatingLocation()
+    }
 }
 
 //MARK: - CLLocationManagerDelegate
@@ -61,7 +94,7 @@ extension HomeViewController: CLLocationManagerDelegate {
         let location = locations[locations.count - 1]
         if location.horizontalAccuracy > 0 {
             locationManager.stopUpdatingLocation()
-            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            //print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
             
             let requestParams: [String : String] = ["lat": String(location.coordinate.latitude),
                                                       "lon": String(location.coordinate.longitude),
--- WeatherPlease/Model/WeatherDataModel.swift
+++ WeatherPlease/Model/WeatherDataModel.swift
@@ -7,3 +7,50 @@
 //
 
 import Foundation
+
+final class WeatherDataModel {
+    
+    var temperature: Int = 0
+    var windSpeed: Int = 0
+    var condition: Int = 0
+    var locationName: String = ""
+    var weatherImageName: String = "unknown"
+    
+    func updateWeatherImage(forConditionID id: Int) -> String {
+        switch id {
+        case 200...232:
+            return "storm"
+        case 300...321:
+            return "drop"
+        case 500...504:
+            return "rain1"
+        case 512...531:
+            return "rain"
+        case 511, 600...622:
+            return "snow"
+        case 701...781:
+            return "clouds2"
+        case 800:
+            return "sun"
+        case 801, 951:
+            return "cloudy4"
+        case 802...804:
+            return "clouds"
+        case 900...902, 957...962:
+            return "tornado"
+        case 903:
+            return "temperature"
+        case 904:
+            return "temperature1"
+        case 905:
+            return "windy1"
+        case 906:
+            return "snow1"
+        case 952...956:
+            return "wind"
+        default:
+            return "unknown"
+        }
+    }
+    
+}
